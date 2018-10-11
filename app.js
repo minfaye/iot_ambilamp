ff939d86ff79ad69e2dc8a4d87532a72f9f1b3d5
minfaye <minfaye@example.com>
2018-10-11 09:58:37 -0700

    made home and details page

---
+++ app.js
@@ -0,0 +1,15 @@
+var express		=require('express'),
+	app			=express();
+
+app.set('port', (process.env.PORT || 3000))
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.render('home');
+});
+
+app.get('/details', (req, res) => {
+	res.render('details');
+});
+
+app.listen(app.get('port'), () => console.log('Listening on port' + app.get('port')));	
