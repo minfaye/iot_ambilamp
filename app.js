f317d580adf7708106ec4ddd1864db5f9afb0a23
minfaye <minfaye@example.com>
2018-10-31 23:17:00 -0700

    App is ready to deploy

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 var express		=require('express'),
+	bodyParser 	=require('body-parser'),
 	app			=express();
+
+
+var indexRoutes = require('./routes/index')
 
 app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/assets'));
-app.get('/', (req, res) => {
-	res.render('home');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/details', (req, res) => {
-	res.render('details');
-});
+/******** ROUTES ********/
+app.use('/', indexRoutes);
+
+
+
 
 app.listen(app.get('port'), () => console.log('Listening on port' + app.get('port')));	
